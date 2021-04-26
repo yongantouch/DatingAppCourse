9dc948df65bc7af40060dbfb013a8173073a574e
yongantouch <lyanchong44@gmail.com>
2021-04-26 18:35:35 +0700

    end of section 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+
+  register(model: any){
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user: User)=>{
+        if(user){
+          this.currentUserSource.next(user);
+          localStorage.setItem('user', JSON.stringify(user));
+        }
+        return user;
+      })
+    )
+  }
+
+  login(model: any){
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: User)=>{
+        const user = response;
+        if(user){
+          this.currentUserSource.next(user);
+          localStorage.setItem('user', JSON.stringify(user));
+          return response;
+        }
+      })
+    );
+  }
+
+  setCurrentUser(user: User){
+    this.currentUserSource.next(user);
+  }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+}
